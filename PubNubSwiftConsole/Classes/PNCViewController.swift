6683d3da4a56a0176bfcfb5f7f8cf2d91f2bf2c0
Jordan Zucker <jordan.zucker@gmail.com>
2016-07-13 14:04:45 -0700

    Continued with pod, initial project

--- PubNubSwiftConsole/Classes/PNCViewController.swift
+++ PubNubSwiftConsole/Classes/PNCViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import Foundation
+import PubNub
 
-public class PNCViewController: UIViewController {
+public class PNCViewController: UIViewController, PNObjectEventListener {
+    
+    var client: PubNub? {
+        didSet {
+            oldValue?.removeListener(self)
+            client?.addListener(self)
+        }
+    }
+    
     
     override public func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
         self.view.backgroundColor = UIColor.redColor()
+    }
+    
+    public func client(client: PubNub, didReceiveStatus status: PNStatus) {
+        print("status")
+    }
+    
+    public func client(client: PubNub, didReceiveMessage message: PNMessageResult) {
+        print("message")
+    }
+    
+    public func client(client: PubNub, didReceivePresenceEvent event: PNPresenceEventResult) {
+        print("presence")
     }
 }
