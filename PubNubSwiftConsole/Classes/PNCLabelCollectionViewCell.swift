3cae9932a0b4cbaa28afac7bd6743d23244d387c
Keith Martin <kmartin82195@gmail.com>
2016-07-20 16:18:39 -0700

    Changed title and contents to titleLabel and contentsLabel and to constants

--- PubNubSwiftConsole/Classes/PNCLabelCollectionViewCell.swift
+++ PubNubSwiftConsole/Classes/PNCLabelCollectionViewCell.swift
@@ -10,28 +10,28 @@ import Foundation
 
 class PNCLabelCollectionViewCell: UICollectionViewCell {
     
-    var title: UILabel
-    var contents: UILabel
+    let titleLabel: UILabel
+    let contentsLabel: UILabel
     
     static func reuseIdentifier() -> String {
         return String(self.dynamicType)
     }
     
     override init(frame: CGRect) {
-        title = UILabel(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height/3))
-        contents = UILabel(frame: CGRect(x: 0, y: title.frame.size.height, width: frame.size.width, height: frame.size.height/3))
+        titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height/3))
+        contentsLabel = UILabel(frame: CGRect(x: 0, y: titleLabel.frame.size.height, width: frame.size.width, height: frame.size.height/3))
     
         super.init(frame: frame)
-        title.textAlignment = .Center
-        title.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
-        title.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(title)
+        titleLabel.textAlignment = .Center
+        titleLabel.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titleLabel)
         
-        
-        contents.textAlignment = .Center
-        contents.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
-        contents.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(contents)
+        contentsLabel.textAlignment = .Center
+        contentsLabel.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
+        contentsLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentsLabel.numberOfLines = 3
+        contentView.addSubview(contentsLabel)
         
         contentView.layer.borderWidth = 3
     }
