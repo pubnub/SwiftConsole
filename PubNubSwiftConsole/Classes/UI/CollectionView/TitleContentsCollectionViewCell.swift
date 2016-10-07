d3d5195ac307b7b35a0bebe561db2734cf9cb1b4
Jordan Zucker <jordan.zucker@gmail.com>
2016-10-07 02:16:21 -0700

    Stopping point, working full proof of concept for collection view

--- PubNubSwiftConsole/Classes/UI/CollectionView/TitleContentsCollectionViewCell.swift
+++ PubNubSwiftConsole/Classes/UI/CollectionView/TitleContentsCollectionViewCell.swift
@@ -10,4 +10,40 @@ import UIKit
 
 class TitleContentsCollectionViewCell: UICollectionViewCell {
     
+    private let titleLabel: UILabel
+    private let contentsLabel: UILabel
+    private let stackView: UIStackView
+    
+    override init(frame: CGRect) {
+        let title = UILabel(frame: .zero)
+        let contents = UILabel(frame: .zero)
+        self.stackView = UIStackView(arrangedSubviews: [title, contents])
+        self.titleLabel = title
+        self.contentsLabel = contents
+        super.init(frame: frame)
+        contentView.addSubview(self.stackView)
+        stackView.forceAutoLayout()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        let views = [
+            "stackView": stackView
+        ]
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[stackView]|", options: [], metrics: nil, views: views)
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[stackView]|", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.activate(verticalConstraints)
+        NSLayoutConstraint.activate(horizontalConstraints)
+        contentView.setNeedsLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func update(title: String, contents: String) {
+        titleLabel.text = title
+        contentsLabel.text = contents
+        contentView.setNeedsLayout()
+    }
+    
 }
--- PubNubSwiftConsole/Classes/UI/ConsoleViewController.swift
+++ PubNubSwiftConsole/Classes/UI/ConsoleViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 import CoreData
 
-open class ConsoleViewController: ViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
+open class ConsoleViewController: ViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, NSFetchedResultsControllerDelegate {
     
     let console: SwiftConsole
-    let tableView: UITableView
+    let collectionView: UICollectionView
     
     lazy var fetchedResultsController: NSFetchedResultsController<Result> = {
         let allResultsFetchRequest: NSFetchRequest<Result> = Result.fetchRequest()
@@ -25,7 +25,9 @@ open class ConsoleViewController: ViewController, UITableViewDelegate, UITableVi
     
     public required init(console: SwiftConsole) {
         let bounds = UIScreen.main.bounds
-        self.tableView = UITableView(frame: .zero, style: .plain)
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = ResultCollectionViewCell.size
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         self.console = console
         super.init()
     }
@@ -42,17 +44,20 @@ open class ConsoleViewController: ViewController, UITableViewDelegate, UITableVi
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.forceAutoLayout()
-        tableView.register(ResultTableViewCell.self, forCellReuseIdentifier: ResultTableViewCell.reuseIdentifier())
-        tableView.rowHeight = ResultTableViewCell.height
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.forceAutoLayout()
+        collectionView.backgroundColor = UIColor.red
+        collectionView.register(TitleContentsCollectionViewCell.self, forCellWithReuseIdentifier: TitleContentsCollectionViewCell.reuseIdentifier())
+        collectionView.register(ResultCollectionViewCell.self, forCellWithReuseIdentifier: ResultCollectionViewCell.reuseIdentifier())
+        //collectionView.register(TitleContentsCollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: TitleContentsCollectionViewCell.reuseIdentifier())
+        //tableView.rowHeight = ResultTableViewCell.height
         let views = [
-            "tableView": tableView,
+            "collectionView": collectionView,
         ]
-        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[tableView]|", options: [], metrics: nil, views: views)
-        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[tableView]|", options: [], metrics: nil, views: views)
+        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[collectionView]|", options: [], metrics: nil, views: views)
+        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[collectionView]|", options: [], metrics: nil, views: views)
         NSLayoutConstraint.activate(verticalConstraints)
         NSLayoutConstraint.activate(horizontalConstraints)
         self.view.setNeedsLayout()
@@ -71,8 +76,8 @@ open class ConsoleViewController: ViewController, UITableViewDelegate, UITableVi
     
     // MARK: - UITableViewDataSource
     
-    func configureCell(cell: UITableViewCell, indexPath: IndexPath) {
-        guard let resultCell = cell as? ResultTableViewCell else {
+    func configureCell(cell: UICollectionViewCell, indexPath: IndexPath) {
+        guard let resultCell = cell as? ResultCollectionViewCell else {
             fatalError()
         }
         let result = fetchedResultsController.object(at: indexPath)
@@ -81,66 +86,132 @@ open class ConsoleViewController: ViewController, UITableViewDelegate, UITableVi
         
     }
     
-    public func numberOfSections(in tableView: UITableView) -> Int {
+    // MARK: - UICollectionViewDataSource
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            guard let onlySectionInfo = fetchedResultsController.sections?.first else {
+                fatalError("No sections in fetchedResultsController")
+            }
+            return onlySectionInfo.numberOfObjects
+        default:
+            fatalError("Can't handle this")
+        }
+    }
+    
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        /*
         guard let sections = fetchedResultsController.sections else {
             fatalError("No sections in fetchedResultsController")
         }
         return sections.count
+ */
+        return 2
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ResultTableViewCell.reuseIdentifier(), for: indexPath)
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleContentsCollectionViewCell.reuseIdentifier(), for: indexPath) as? TitleContentsCollectionViewCell else {
+                fatalError("Unexpected cell type")
+            }
+            cell.update(title: "Channels", contents: "a, c")
+            return cell
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultCollectionViewCell.reuseIdentifier(), for: indexPath)
+            var adjustedIndexPath = indexPath
+            adjustedIndexPath.section -= 1
+            configureCell(cell: cell, indexPath: adjustedIndexPath)
+            return cell
+        default:
+            fatalError("Can't handle section 3")
+        }
+        /*
+        var adjustedIndexPath = indexPath
+        adjustedIndexPath.section += 1
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultCollectionViewCell.reuseIdentifier(), for: adjustedIndexPath)
         configureCell(cell: cell, indexPath: indexPath)
         return cell
+ */
     }
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let sections = fetchedResultsController.sections else {
-            fatalError("No sections in fetchedResultsController")
+    public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionElementKindSectionHeader:
+            guard let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: TitleContentsCollectionViewCell.reuseIdentifier(), for: indexPath) as? TitleContentsCollectionViewCell else {
+                fatalError("Can't handle other types right now")
+            }
+            cell.update(title: "Channels", contents: "1")
+            return cell
+        default:
+            fatalError("Can't deal with anything else but headers")
         }
-        let sectionInfo = sections[section]
-        return sectionInfo.numberOfObjects
     }
     
-    // MARK: - UITableViewDelegate
+    // MARK: - UICollectionViewDelegate
     
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let alertController = UIAlertController(title: "Change channel", message: "Enter new channels, or remove some, comma separated", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter channels ..."
+        }
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            guard let textFieldText = alertController.textFields?[0].text else {
+                return
+            }
+            self.console.client.subscribeToChannels([textFieldText], withPresence: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+        
     // MARK: - NSFetchedResultsControllerDelegate
     
-    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-    
     public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-        case .delete:
-            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-        case .move:
-            break
-        case .update:
-            break
-        }
+        collectionView.performBatchUpdates({ 
+            switch type {
+            case .insert:
+                var adjustedSectionIndex = sectionIndex
+                adjustedSectionIndex += 1
+                self.collectionView.insertSections(IndexSet(integer: adjustedSectionIndex))
+            case .delete:
+                var adjustedSectionIndex = sectionIndex
+                adjustedSectionIndex += 1
+                self.collectionView.deleteSections(IndexSet(integer: adjustedSectionIndex))
+            case .move:
+                break
+            case .update:
+                break
+            }
+            })
+        
     }
     
     public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            tableView.insertRows(at: [newIndexPath!], with: .fade)
-        case .delete:
-            tableView.deleteRows(at: [indexPath!], with: .fade)
-        case .update:
-            guard let cell = tableView.cellForRow(at: indexPath!) else {
-                fatalError()
+        var adjustedIndexPath = indexPath
+        adjustedIndexPath?.section += 1
+        var adjustedNewIndexPath = newIndexPath
+        adjustedNewIndexPath?.section += 1
+        collectionView.performBatchUpdates({ 
+            switch type {
+            case .insert:
+                self.collectionView.insertItems(at: [adjustedNewIndexPath!])
+            case .delete:
+                self.collectionView.deleteItems(at: [adjustedIndexPath!])
+            case .update:
+                guard let cell = self.collectionView.cellForItem(at: adjustedIndexPath!) else {
+                    fatalError()
+                }
+                self.configureCell(cell: cell, indexPath: adjustedIndexPath!)
+            case .move:
+                self.collectionView.moveItem(at: adjustedIndexPath!, to: adjustedNewIndexPath!)
             }
-            configureCell(cell: cell, indexPath: indexPath!)
-        case .move:
-            tableView.moveRow(at: indexPath!, to: newIndexPath!)
-        }
-    }
-    
-    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
+            })
     }
 
 }
